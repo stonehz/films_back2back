208c381f3897a352f3935ac0666ac587dc6c5a18
stonehz <stonehz@gmail.com>
2014-02-01 20:41:05 +0000

    finding times for the movies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
 
   def films
     @date = params[:cinelist][:date].try(:gsub, "/", "")
+    @cinema = params[:cinelist][:cinema_id]
     opts = {cinema: params[:cinelist][:cinema_id], date: @date}
     @films = Cinelist.reach("f", opts)
   end
@@ -14,5 +15,15 @@ class HomeController < ApplicationController
 
     # at this point , I have the edi, title, date,time and need to check
     puts params
+    static_opts = {cinema: params[:movies][:cinema_id], date: params[:movies][:date]}
+    @film_times = {}
+    params[:movies][:results].each_pair do |movie_edi, movie_title|
+      static_opts[:film]=movie_edi
+      temp_times = Cinelist.reach("p", static_opts)
+      temp_times.select! { |perf| perf["available"] == true && Cinelist.t(perf["time"])>=Cinelist.t(params[:movies][:time]) }
+      @film_times[movie_edi] = [movie_title, temp_times.collect { |perf| perf["time"] }]
+    end
+    #example output
+    #"movies"=>{"date"=>"20140201", "time"=>"19:40", "results"=>{"144628"=>["The Wolf of Wall Street"], "109377"=>["12 Years A Slave"], "61662"=>["2D - I, Frankenstein"]}}
   end
 end
