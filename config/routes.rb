910fcc79f96721efa9d3dac551b4b6ac6d81905b
stonehz <stonehz@gmail.com>
2014-03-08 11:55:40 +0000

    removing series for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 MoviesBack2Back::Application.routes.draw do
 
 
-  authenticated :user do
-    root :to => 'home#index'
+  #authenticated :user do
+  root :to => 'home#index'
     post "films", :to => 'home#films', as: :films
     post "results", :to => 'home#film_results', as: :results
     post 'quickview', :to => 'home#favourites', as: :quickview
     #resources :cinelists
-  end
+    # end
   root :to => "home#index"
   devise_for :users
   resources :users
